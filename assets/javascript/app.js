aa85ae086bb817063d5bc94e706402e4d6ba2842
JesusColunga <47442182+JesusColunga@users.noreply.github.com>
2019-04-02 00:03:38 -0600

    1/Apr/2019 24:03

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+/* app.js                      */
+/* Unit 7: Rock Paper Scissors */
+/* 1/Apr/2019                  */
+
+
+// GLOBAL VARIABLES
+// =======================================================================================
+
+  var config = {  // Initialize Firebase
+    apiKey: "AIzaSyCXUHcw46bHzkX4kYQ0RjjZN-DHc-RNlAs",
+    authDomain: "bootcamp-test-firebase.firebaseapp.com",
+    databaseURL: "https://bootcamp-test-firebase.firebaseio.com",
+    projectId: "bootcamp-test-firebase",
+    storageBucket: "bootcamp-test-firebase.appspot.com",
+    messagingSenderId: "507271537393"
+  };
+
+
+// OBJECTS
+// =======================================================================================
+
+
+// FUNCTIONS (Definition)
+// =======================================================================================
+
+
+// FUNCTION CALLS (Execution)
+// =======================================================================================
+firebase.initializeApp(config);  // Initialize Firebase
+
+$(document).ready(function() {
+	
+
+}); // document.ready
